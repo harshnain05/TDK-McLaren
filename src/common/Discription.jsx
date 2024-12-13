14060608d430687b60a536701655a5bb56aa5d6e
harshnain05 <harshnain05@gmail.com>
2024-12-13 15:10:14 +0530

    discription text size adjust

--- src/common/Discription.jsx
+++ src/common/Discription.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Description = ({ text, customColor, myOpacity }) => {
     return (
-        <p className={`text-[22px] max-md:text-sm  ${customColor} ${myOpacity} leading-[29.9px]`}>{text}</p>
+        <p className={`text-[22px] max-md:text-sm  ${customColor} ${myOpacity} max-sm:leading-[21px] max-sm:text-base  leading-[29.9px]`}>{text}</p>
     )
 }
 export default Description
--- src/common/SubHeading.jsx
+++ src/common/SubHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SubHeading = ({ text, spanText, secondSpanText }) => {
     return (
-        <h2 className='font-bold text-[61px] leading-[83px] max-lg:text-[40px] max-lg:leading-[61px] max-md:text-4xl max-sm:text-2xl'>{text}</h2>
+        <h2 className='font-bold text-[61px] leading-[83px]  max-lg:leading-[40px] max-md:text-4xl max-sm:text-3xl'>{text}</h2>
     )
 }
 export default SubHeading
